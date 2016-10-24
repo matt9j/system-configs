4a4bdbe8370dc4cdc7457965fc14d896811ab828
Matt Johnson <matt9j@cs.washington.edu>
2016-10-24 14:43:26 -0700

    emacs: Add support for esther & matt c style

--- emacs/common-startup.el
+++ emacs/common-startup.el
@@ -173,9 +173,88 @@
           (c-put-font-lock-face start (point) 'font-lock-comment-face)))))
   nil)
 
+(c-add-style "e-and-m-c"
+             '((c-basic-offset . 4)     ; Guessed value
+               (c-offsets-alist
+                (block-close . 0)       ; Guessed value
+                (brace-list-close . 0)  ; Guessed value
+                (brace-list-entry . 0)  ; Guessed value
+                (brace-list-intro . +)  ; Guessed value
+                (brace-list-open . 0)   ; Guessed value
+                (case-label . +)        ; Guessed value
+                (class-close . 0)       ; Guessed value
+                (class-open . 0)        ; Guessed value
+                (defun-block-intro . +) ; Guessed value
+                (defun-close . 0)       ; Guessed value
+                (defun-open . 0)        ; Guessed value
+                (else-clause . 0)       ; Guessed value
+                (inclass . +)           ; Guessed value
+                (statement . 0)             ; Guessed value
+                (statement-block-intro . +) ; Guessed value
+                (statement-case-open . 0) ; Guessed value
+                (statement-cont . +)      ; Guessed value
+                (substatement-open . 0)   ; Guessed value
+                (topmost-intro . 0)       ; Guessed value
+                (access-label . -)
+                (annotation-top-cont . 0)
+                (annotation-var-cont . +)
+                (arglist-close . c-lineup-close-paren)
+                (arglist-cont c-lineup-gcc-asm-reg 0)
+                (arglist-cont-nonempty . c-lineup-arglist)
+                (arglist-intro . c-lineup-arglist-intro-after-paren)
+                (block-open . 0)
+                (brace-entry-open . 0)
+                (c . c-lineup-C-comments)
+                (catch-clause . 0)
+                (comment-intro . c-lineup-comment)
+                (composition-close . 0)
+                (composition-open . 0)
+                (cpp-define-intro c-lineup-cpp-define +)
+                (cpp-macro . -1000)
+                (cpp-macro-cont . +)
+                (do-while-closure . 0)
+                (extern-lang-close . 0)
+                (extern-lang-open . 0)
+                (friend . 0)
+                (func-decl-cont . +)
+                (incomposition . +)
+                (inexpr-class . +)
+                (inexpr-statement . +)
+                (inextern-lang . +)
+                (inher-cont . c-lineup-multi-inher)
+                (inher-intro . +)
+                (inlambda . c-lineup-inexpr-block)
+                (inline-close . 0)
+                (inline-open . 0)
+                (inmodule . +)
+                (innamespace . +)
+                (knr-argdecl . 0)
+                (knr-argdecl-intro . 5)
+                (label . 0)
+                (lambda-intro-cont . +)
+                (member-init-cont . c-lineup-multi-inher)
+                (member-init-intro . +)
+                (module-close . 0)
+                (module-open . 0)
+                (namespace-close . 0)
+                (namespace-open . 0)
+                (objc-method-args-cont . c-lineup-ObjC-method-args)
+                (objc-method-call-cont c-lineup-ObjC-method-call-colons c-lineup-ObjC-method-call +)
+                (objc-method-intro .
+                                   [0])
+                (statement-case-intro . +)
+                (stream-op . c-lineup-streamop)
+                (string . -1000)
+                (substatement . +)
+                (substatement-label . 0)
+                (template-args-cont c-lineup-template-args +)
+                (topmost-intro-cont first c-lineup-topmost-intro-cont c-lineup-gnu-DEFUN-intro-cont))))
+
 (defun my-c-mode-common-hook ()
   (font-lock-add-keywords
    nil
-   '((my-c-mode-font-lock-if0 (0 font-lock-comment-face prepend))) 'add-to-end))
+   '((my-c-mode-font-lock-if0 (0 font-lock-comment-face prepend))) 'add-to-end)
+  (c-set-style "e-and-m-c"))
+
 
 (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
