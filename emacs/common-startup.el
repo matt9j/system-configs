41bdb903960ade02654fef6b1aa8e6f4dd80142a
Matt Johnson <matt9j@cs.washington.edu>
2017-01-15 22:57:19 -0800

    emacs: Temporarly disable flymake

--- emacs/common-startup.el
+++ emacs/common-startup.el
@@ -54,18 +54,18 @@
 
 ;; Script that flymake uses to check code. This script must be
 ;; present in the system path.
-(setq pycodechecker "pylint_etc_wrapper.py")
+;;(setq pycodechecker "pylint_etc_wrapper.py")
 
-(when (load "flymake" t)
-  (defun flymake-pycodecheck-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list pycodechecker (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pycodecheck-init)))
+;;(when (load "flymake" t)
+;;  (defun flymake-pycodecheck-init ()
+;;    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;;                       'flymake-create-temp-inplace))
+;;           (local-file (file-relative-name
+;;                        temp-file
+;;                        (file-name-directory buffer-file-name))))
+;;      (list pycodechecker (list local-file))))
+;;  (add-to-list 'flymake-allowed-file-name-masks
+;;               '("\\.py\\'" flymake-pycodecheck-init)))
 
 (add-hook 'python-mode-hook 'flymake-mode)
 
