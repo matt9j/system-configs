530d93fc9ade48d858734af5c32b55ad7e6d447a
Matt Johnson <matt9j@cs.washington.edu>
2018-05-31 11:13:34 -0700

    emacs: Remove unmaintained python flymake

--- emacs/common-startup.el
+++ emacs/common-startup.el
@@ -70,23 +70,6 @@
 (require 'flymake)
 (load-library "flymake-cursor")
 
-;; Script that flymake uses to check code. This script must be
-;; present in the system path.
-;;(setq pycodechecker "pylint_etc_wrapper.py")
-
-;;(when (load "flymake" t)
-;;  (defun flymake-pycodecheck-init ()
-;;    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-;;                       'flymake-create-temp-inplace))
-;;           (local-file (file-relative-name
-;;                        temp-file
-;;                        (file-name-directory buffer-file-name))))
-;;      (list pycodechecker (list local-file))))
-;;  (add-to-list 'flymake-allowed-file-name-masks
-;;               '("\\.py\\'" flymake-pycodecheck-init)))
-
-(add-hook 'python-mode-hook 'flymake-mode)
-
 ;;Start emacs server so new windows open in existing frame
 (load "server")
 (unless (server-running-p) (server-start))
